9b7bd97cad4460ac303114bea2b9a2766e488741
Brian Winkers <bwinkers@gmail.com>
2024-06-20 17:01:40 -0700

    Shorten filename timestamps

--- app/src/stores/nugget.js
+++ app/src/stores/nugget.js
@@ -8,13 +8,34 @@ import { useObservable } from "@vueuse/rxjs";
 
 export const useNuggetStore = defineStore("nugget", () => {
 
+  // STATE ////////////////////////
+
+
+  // GETTERS /////////////////////
+  const getFilenameDate = () => {
+    return new Date().toISOString()
+    .replaceAll('-', '')
+    .replaceAll('T', '_')
+    .replaceAll(':', '')
+    .replaceAll('.', '_')
+    .replaceAll('Z', '');
+  }
+
+  // ACTIONS //////////////////////
+
   // Create Nugget
   const createNugget = async (nuggetData) => {
-    const cleanNugget = resolveJSON(nuggetData);
+    const cleanNugget = resolveJSON(nuggetData.data);
     cleanNugget.dateCreated = new Date().toISOString();
     cleanNugget.dateModified = null;
 
     const newId = await db.nuggets.add(cleanNugget);
+
+    console.log('Selected Files', nuggetData.selectedFiles)
+    console.log('Captured Images', nuggetData.capturedImages)
+    console.log('Recorded Videos', nuggetData.videoRecordings)
+    console.log('Recorded Audio', nuggetData.audioRecordings)
+
     return newId;
   }
 
@@ -24,6 +45,12 @@ export const useNuggetStore = defineStore("nugget", () => {
   }
 
   return {
+    // State
+
+    // Getters
+    getFilenameDate,
+
+    // Actions
     createNugget,
     resolveJSON
   }
