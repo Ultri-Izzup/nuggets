596088e556b35f6a3982c995449fb84e5eed3a5b
Brian Winkers <bwinkers@gmail.com>
2024-06-21 14:59:59 -0700

    Move code out of the actual store by referencing it from elsewhere

--- app/src/stores/nugget.js
+++ app/src/stores/nugget.js
@@ -1,57 +1,32 @@
-import { ref, computed } from "vue";
+// import { ref, computed } from "vue";
 import { defineStore } from "pinia";
-import { useStorage } from "@vueuse/core";
+// import { useStorage } from "@vueuse/core";
 
-import { db } from "../dexie/db.js";
-import { liveQuery } from "dexie";
-import { useObservable } from "@vueuse/rxjs";
+import { useNuggets } from "@/composables/Nuggets";
+
+const Nug = useNuggets();
 
 export const useNuggetStore = defineStore("nugget", () => {
 
-  // STATE ////////////////////////
+  // STATE
 
 
-  // GETTERS /////////////////////
-  const getFilenameDate = () => {
-    return new Date().toISOString()
-    .replaceAll('-', '')
-    .replaceAll('T', '_')
-    .replaceAll(':', '')
-    .replaceAll('.', '_')
-    .replaceAll('Z', '');
-  }
+  // GETTERS / CALCULATED STATE
+  const newFileTimestamp = Nug.newFileTimestamp
 
-  // ACTIONS //////////////////////
+  // ACTIONS / FUNCTIONS
+  const createNugget = Nug.createNugget;
 
-  // Create Nugget
-  const createNugget = async (nuggetData) => {
-    const cleanNugget = resolveJSON(nuggetData.data);
-    cleanNugget.dateCreated = new Date().toISOString();
-    cleanNugget.dateModified = null;
-
-    const newId = await db.nuggets.add(cleanNugget);
-
-    console.log('Selected Files', nuggetData.selectedFiles)
-    console.log('Captured Images', nuggetData.capturedImages)
-    console.log('Recorded Videos', nuggetData.videoRecordings)
-    console.log('Recorded Audio', nuggetData.audioRecordings)
-
-    return newId;
-  }
-
-  // Resolve a JavaScript object with possible refs to plain JSON
-  const resolveJSON = (data) => {
-    return JSON.parse(JSON.stringify(data));
-  }
+  const resolveToJSON = Nug.resolveToJSON;
 
   return {
     // State
 
     // Getters
-    getFilenameDate,
+    newFileTimestamp,
 
-    // Actions
+    // Actions/Functions
     createNugget,
-    resolveJSON
+    resolveToJSON
   }
 });
