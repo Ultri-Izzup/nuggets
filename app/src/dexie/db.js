bceee7d8a4e704ee0f85c097cd5e1f166af181bb
Brian Winkers <bwinkers@gmail.com>
2024-06-20 17:02:17 -0700

    store extension

--- app/src/dexie/db.js
+++ app/src/dexie/db.js
@@ -1,7 +1,7 @@
 import Dexie from 'dexie';
 
 export const db = new Dexie('nuggetData');
-db.version(1).stores({
+db.version(2).stores({
   nuggets: '++id, name, dateCreated, dateModified, *tags', // Primary key and indexed props
-  files: 'id, nuggetId, directoryPath, fileName, dateCreated, dateModified, *tags',
+  files: 'id, nuggetId, directoryPath, fileName, ext, dateCreated, dateModified, *tags',
 });
