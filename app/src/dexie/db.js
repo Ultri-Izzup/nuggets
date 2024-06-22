d10adff9fdd9c2eb6ae4636e9d1cc81a20e1337d
Brian Winkers <bwinkers@gmail.com>
2024-06-21 22:33:52 -0700

    remove files, store as nuggets

--- app/src/dexie/db.js
+++ app/src/dexie/db.js
@@ -2,7 +2,6 @@ import Dexie from 'dexie';
 Dexie.debug = true;
 
 export const db = new Dexie('nuggetData');
-db.version(3).stores({
-  nuggets: '++id, name, dateCreated, dateModified, *tags', // Primary key and indexed props
-  files: 'id, nuggetId, directoryPath, fileName, ext, dateCreated, dateModified, *tags',
+db.version(4).stores({
+  nuggets: '++id, name, dateCreated, dateModified, *tags, parentNuggetId', // Primary key and indexed props
 });
