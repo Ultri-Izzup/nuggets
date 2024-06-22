8855c6bf7c1375a6c12c934ad0d5f57b21a36279
Brian Winkers <bwinkers@gmail.com>
2024-06-22 16:07:13 -0700

    Moved shared funcs and constants from composables to root

--- app/src/dexie/db.js
+++ app/src/dexie/db.js
@@ -2,6 +2,9 @@ import Dexie from 'dexie';
 Dexie.debug = true;
 
 export const db = new Dexie('nuggetData');
-db.version(4).stores({
-  nuggets: '++id, name, dateCreated, dateModified, *tags, parentNuggetId', // Primary key and indexed props
+db.version(12).stores({
+  nuggets: '++id, name, createdAt, updatedAt, *tags',
+  assets: '[directoryPath+fileName], nuggetId',
+  geoPoints: '++id, nuggetId, lat, long',
+  relations: '++id, [nuggetId+relationTypeId], relatedId',
 });
