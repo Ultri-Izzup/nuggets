e5fdcda4d894ef36bec3278424c01b81acd5a919
Brian Winkers <bwinkers@gmail.com>
2024-06-23 08:33:18 -0700

    Uploade files listed

--- app/src/components/NuggetEdit.vue
+++ app/src/components/NuggetEdit.vue
@@ -9,9 +9,7 @@
       <v-container v-if="nuggetData" max-width="600">
         <v-row>
           <v-col cols="12">
-            <v-row class="text-body">
-              {{ nuggetData.name }} &nbsp;
-            </v-row>
+            <v-row class="text-body"> {{ nuggetData.name }} &nbsp; </v-row>
             <v-row class="mt-1 pt-0 text-caption text-grey"> Name </v-row>
           </v-col>
         </v-row>
@@ -20,16 +18,38 @@
             <v-row class="text-body">
               {{ nuggetData.description }} &nbsp;
             </v-row>
-            <v-row class="mt-1 pt-0 text-caption text-grey"> Description </v-row>
+            <v-row class="mt-1 pt-0 text-caption text-grey">
+              Description
+            </v-row>
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12">
             <v-row class="text-body">
-              {{ nuggetData.tags.join(',') }} &nbsp;
+              {{ nuggetData.tags.join(",") }} &nbsp;
             </v-row>
             <v-row class="mt-1 pt-0 text-caption text-grey"> Tags </v-row>
           </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" class="text-h6">Files</v-col>
+          <v-col cols="12" md="6" v-for="(file, index) in files" :key="index" class="text-caption py-0">
+            {{ file.fileName }}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" class="text-h6">Images</v-col>
+          <v-row v-for="(file, index) in capturedImages" :key="index" class="text-body-2">
+            <v-col>
+              <v-row class="align-center justify-center">
+                <v-divider></v-divider>
+                <v-col>
+                  <img :src="file.dataURL" width="100%" />
+                  <span class="text-caption">{{ file.name }}</span>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
         </v-row>
       </v-container>
     </v-responsive>
@@ -39,22 +59,21 @@
 <script setup>
 import { ref, watch } from "vue";
 
-import { useRoute, useRouter } from "vue-router";
-
 import { useNuggetStore } from "@/stores/nugget";
+
+const props = defineProps(["nuggetId"]);
+
 const nug = useNuggetStore();
 
-const route = useRoute();
-const nuggetId = ref(route.params.id);
-
 const nuggetData = ref();
+const files = ref();
 
 watch(
-  () => route.params.id,
+  () => props.nuggetId,
   async (newVal, oldVal) => {
     if (newVal) {
-      const reqNug = await nug.getNugget(newVal);
-      nuggetData.value = reqNug;
+      nuggetData.value = await nug.getNugget(newVal);
+      files.value = await nug.getNuggetFiles(newVal);
     }
   },
   { immediate: true }
