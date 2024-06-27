ec5683e73e31e82535ab52c0672e5f525b86d57a
Brian Winkers <bwinkers@gmail.com>
2024-06-26 17:20:56 -0700

    Saving images through dataURK worker is functional

--- app/public/workers/dataURLWorker.js
+++ app/public/workers/dataURLWorker.js
@@ -3,60 +3,54 @@ import { db } from "../src/dexie/db.js";
 import {fileNameRegex, getSyncFileHandle} from './shared/opfs.js';
 
 self.onmessage = async (msg) => {
-  console.log("dataURL WORKER MESSAGE RECEIVED", msg);
-  console.log("data", msg.data);
-  console.log("dataURLs", msg.data.dataURLObjs);
+  // console.log("dataURL WORKER MESSAGE RECEIVED", msg);
+  // console.log("data", msg.data);
+  // console.log("dataURLs", msg.data.dataURLObjs);
 
-  // if (msg.data.nuggetId && msg.data.subDir && msg.data.dataObjs ) {
-  //   for(const dataObj of msg.data.dataObjs) {
-  //     console.log('dataObj', dataObj);
+  if (msg.data.nuggetId && msg.data.subDir && msg.data.dataURLObjs ) {
+    for(const dataObj of msg.data.dataURLObjs) {
+      console.log('dataObj', dataObj);
 
-  //       const fileName = dataObj.name.replace(fileNameRegex, "-");
+        const fileName = dataObj.name.replace(fileNameRegex, "-");
 
-  //       // Matches Dexie/IndexedDB key value
-  //       const keyPath = `${msg.data.nuggetId}/${msg.data.subDir}/${fileName}`;
+        // Matches Dexie/IndexedDB key value
+        const keyPath = `${msg.data.nuggetId}/${msg.data.subDir}/${fileName}`;
 
-  //       const fullPath = `nugget/${keyPath}`
-  //       const opfsFH = await getSyncFileHandle(fullPath)
+        const fullPath = `nugget/${keyPath}`
+        const opfsFH = await getSyncFileHandle(fullPath)
 
-  //       // const fileData = await sysFH.getFile();
-  //       // const contents = await fileData.arrayBuffer();
+        // const fileData = await sysFH.getFile();
+        // const contents = await fileData.arrayBuffer();
 
-  //       const fetched = await fetch(dataObj.dataURL);
-  //       const contents = await fetched.blob();
+        const fetched = await fetch(dataObj.dataURL);
+        const cBlob = await fetched.blob();
+        const contents = await cBlob.arrayBuffer();
 
-  //       opfsFH.write(contents, { at: 0 });
-  //       opfsFH.flush();
-  //       const newSize = opfsFH.getSize();
-  //       opfsFH.close();
+        opfsFH.write(contents, { at: 0 });
+        opfsFH.flush();
+        const newSize = opfsFH.getSize();
+        opfsFH.close();
 
-  //       const assetMeta = {
-  //         nuggetId: msg.data.nuggetId,
-  //         subDir: msg.data.subDir,
-  //         fileName: fileName,
-  //         mimeType: fileData.type,
-  //         dateCreated: new Date().toISOString(),
-  //         fileSize: newSize
-  //       };
+        const assetMeta = {
+          nuggetId: msg.data.nuggetId,
+          subDir: msg.data.subDir,
+          fileName: fileName,
+          mimeType: cBlob.type,
+          dateCreated: new Date().toISOString(),
+          fileSize: newSize
+        };
 
-  //       console.log('META', assetMeta)
+        console.log('META', assetMeta)
 
-  //       const newId = await db.assets.put(assetMeta);
-  //       console.log("NEW FILE", newId);
+        const newId = await db.assets.put(assetMeta);
+        console.log("NEW FILE", newId);
 
-  //       const response = {
-  //         fileId: newId,
-  //         meta: assetMeta
-  //       };
+        const response = {
+          fileId: newId,
+          meta: assetMeta
+        };
 
-  //       self.postMessage({ responseData: response });
-
-  //   }
-  // }
-
-  // const handler = msg.data.targetHandler;
-
-  // console.log("USE HANDLER", handler, msgHandlers);
-
-  // await msgHandlers[handler](msg.data.targetData, msg.data.responseHandler);
+        self.postMessage({ responseData: response });
+    }
+  }
 };
