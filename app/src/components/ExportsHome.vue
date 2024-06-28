cd76a1c1b734c1acf03f01c89c05b602772f5423
Brian Winkers <bwinkers@gmail.com>
2024-06-28 16:05:56 -0700

    placeholder for exports

---
+++ app/src/components/ExportsHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="flex">
+    <v-responsive class="align-centerfill-height mx-auto py-6" max-width="900">
+      <div class="text-center">
+        <h1 class="text-h3 font-weight-bold">Exports</h1>
+      </div>
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+import { useNuggetStore } from "../stores/nugget";
+const nug = useNuggetStore();
+
+
+</script>
