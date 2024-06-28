3de13f16cd5fe62ade79dc2ca700dcf30f65b7c5
Brian Winkers <bwinkers@gmail.com>
2024-06-28 16:06:41 -0700

    Added exports

--- app/src/dexie/db.js
+++ app/src/dexie/db.js
@@ -2,9 +2,10 @@ import Dexie from 'dexie';
 Dexie.debug = true;
 
 export const db = new Dexie('nuggetData');
-db.version(20).stores({
+db.version(23).stores({
   nuggets: '++id, name, createdAt, updatedAt, *tags',
   assets: '[nuggetId+subDir+fileName], mimeType',
   relations: '++id, [nuggetId+relationTypeId], relatedId',
   opfsHandles: 'handlePath, type',
+  exports: '++id, nuggetId, createdAt, completedAt'
 });
