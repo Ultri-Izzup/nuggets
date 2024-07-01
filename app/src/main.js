8c2f10e9987a92b1db59ab9948e23f5343bd641b
Brian Winkers <bwinkers@gmail.com>
2024-07-01 03:43:39 -0700

    i18n support

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@
  *
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
+import messages from "@intlify/unplugin-vue-i18n/messages";
 
 // Plugins
 import { registerPlugins } from '@/plugins'
@@ -12,8 +13,18 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  legacy: false, // you must set `false`, to use Composition API
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: messages
+})
 
 const app = createApp(App)
+
+app.use(i18n)
 
 registerPlugins(app)
 
