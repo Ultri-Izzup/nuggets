d28c998dcd56803b42bb78ad5f18a4f35a1939d3
Brian Winkers <bwinkers@gmail.com>
2024-07-05 01:06:33 -0700

    store full relation type, not just the ID

--- app/src/dexie/db.js
+++ app/src/dexie/db.js
@@ -2,10 +2,10 @@ import Dexie from 'dexie';
 Dexie.debug = true;
 
 export const db = new Dexie('nuggetData');
-db.version(23).stores({
+db.version(24).stores({
   nuggets: '++id, name, createdAt, updatedAt, *tags',
   assets: '[nuggetId+subDir+fileName], mimeType',
-  relations: '++id, [nuggetId+relationTypeId], relatedId',
+  relations: '++id, [nuggetId+relationType], relatedId',
   opfsHandles: 'handlePath, type',
   exports: '++id, nuggetId, createdAt, completedAt'
 });
